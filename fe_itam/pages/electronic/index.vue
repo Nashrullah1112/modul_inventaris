087829a5f7a153a0c0a368b923c8cc57280dfc46
faiqnoov <mfaiqn@gmail.com>
2024-12-01 20:15:27 +0700

    patch: adjust electronic data list

--- fe_itam/pages/electronic/index.vue
+++ fe_itam/pages/electronic/index.vue
@@ -5,9 +5,13 @@ import { ArrowUpDown } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import type { ColumnDef } from "@tanstack/vue-table";
-
+import { useToast } from '@/components/ui/toast/use-toast'
+import ActionBtnEdit from "~/components/atoms/ActionBtnEdit.vue";
+import ActionBtnDelete from "~/components/atoms/ActionBtnDelete.vue";
 
 const config = useRuntimeConfig();
+const router = useRouter();
+const { toast } = useToast()
 
 interface Device {
   id: number;
@@ -47,50 +51,6 @@ interface Electronic {
   division: string;
 }
 
-//state for application data
-const devices = ref<Device[]>([]);
-const isLoading = ref(false);
-const errorMessage = ref<string | null>(null);
-
-async function fetchDevices() {
-  try {
-    isLoading.value = true;
-    errorMessage.value = null;
-
-    const response = await $fetch<{
-      message: string;
-      data: Device[];
-      error: any;
-    }>(config.public.API_URL + "/asset-perangkat", {
-      method: "GET",
-    });
-
-    if (response.error) {
-      throw new Error(response.error);
-    }
-    // Extract the data array from the response
-    devices.value = response.data;
-  } catch (error) {
-    console.error("Error fetching devices:", error);
-    errorMessage.value = "Failed to load device data. Please try again.";
-  } finally {
-    isLoading.value = false;
-  }
-}
-
-onMounted(fetchDevices);
-
-// Transform API data for table usage
-const electronics = computed(() =>
-  devices.value.map((device) => ({
-    id: device.id,
-    serialNumber: device.serial_number,
-    assetNumber: device.asset_id.toString(),
-    deviceName: device.model,
-    division: device.divisi_id.toString(),
-  }))
-);
-
 const columns: ColumnDef<Electronic>[] = [
   {
     id: "select",
@@ -111,7 +71,33 @@ const columns: ColumnDef<Electronic>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "serialNumber",
+    accessorKey: "model",
+    header: ({ column }) => {
+      return h(
+        Button,
+        {
+          variant: "ghost",
+          onClick: () => column.toggleSorting(column.getIsSorted() === "asc"),
+        },
+        () => ["Model", h(ArrowUpDown, { class: "ml-2 h-4 w-4" })]
+      );
+    },
+  },
+  {
+    accessorKey: "tipe_aset",
+    header: ({ column }) => {
+      return h(
+        Button,
+        {
+          variant: "ghost",
+          onClick: () => column.toggleSorting(column.getIsSorted() === "asc"),
+        },
+        () => ["Tipe Asset", h(ArrowUpDown, { class: "ml-2 h-4 w-4" })]
+      );
+    },
+  },
+  {
+    accessorKey: "serial_number",
     header: ({ column }) => {
       return h(
         Button,
@@ -123,10 +109,10 @@ const columns: ColumnDef<Electronic>[] = [
       );
     },
     cell: ({ row }) =>
-      h("div", { class: "capitalize" }, row.getValue("serialNumber")),
+      h("div", { class: "capitalize" }, row.getValue("serial_number")),
   },
   {
-    accessorKey: "assetNumber",
+    accessorKey: "asset_id",
     header: ({ column }) => {
       return h(
         Button,
@@ -137,17 +123,19 @@ const columns: ColumnDef<Electronic>[] = [
         () => ["Nomor Asset", h(ArrowUpDown, { class: "ml-2 h-4 w-4" })]
       );
     },
-    cell: ({ row }) => h("div", {}, row.getValue("assetNumber")),
+    cell: ({ row }) => h("div", {}, row.getValue("asset_id")),
   },
   {
-    accessorKey: "deviceName",
-    header: () => h("div", {}, "Data Perangkat"),
-    cell: ({ row }) => h("div", {}, row.getValue("deviceName")),
-  },
-  {
-    accessorKey: "division",
+    accessorKey: "divisi_id",
     header: () => h("div", {}, "Divisi"),
-    cell: ({ row }) => h("div", {}, row.getValue("division")),
+  },
+  {
+    accessorKey: "hasil_pemeriksaan_aset",
+    header: () => h("div", {}, "Hasil Pemeriksaan"),
+  },
+  {
+    accessorKey: "status_aset",
+    header: () => h("div", {}, "Status Asset"),
   },
   {
     id: "actions",
@@ -155,20 +143,16 @@ const columns: ColumnDef<Electronic>[] = [
     cell: ({ row }) => {
       return h("div", { class: "flex gap-2" }, [
         h(
-          Button,
+          ActionBtnEdit,
           {
-            variant: "default",
-            class: "bg-blue-500 hover:bg-blue-600",
-            onClick: () => console.log("Update", row.original.id),
+            onClick: () => router.push(`/electronic/${row.original.id}/edit`),
           },
           () => "Update"
         ),
         h(
-          Button,
+          ActionBtnDelete,
           {
-            variant: "default",
-            class: "bg-red-500 hover:bg-red-600",
-            onClick: () => console.log("Delete", row.original.id),
+            onClick: () => deleteData(row.original.id),
           },
           () => "Delete"
         ),
@@ -176,23 +160,47 @@ const columns: ColumnDef<Electronic>[] = [
     },
   },
 ];
+
+// fetch assets list
+const {
+  data: result,
+  status,
+  refresh,
+} = await useFetch(config.public.API_URL + "/asset-perangkat");
+
+// Delete action
+async function deleteData(id: number) {
+  try {
+    const { status } = await useFetch(config.public.API_URL + `/asset-perangkat/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (status.value == 'success') {
+      toast({
+        title: 'Success',
+        description: 'Data deleted successfully.',
+      })
+      refresh()
+    } else {
+      toast({
+        title: 'Failed',
+        description: `Error when deleting data`,
+      })
+    }
+  } catch (error) {
+    console.error('Error occured:', error);
+  }
+}
 </script>
 
 <template>
-  <div class="container mx-auto py-10">
-    <div class="rounded-md border">
-      <div class="p-6 border-b">
-        <div class="flex items-center justify-between">
-          <h1 class="text-lg font-medium">Data Aset Elektronik</h1>
-          <Button variant="outline" @click="router.push('/electronic/add')">
-            Tambah Data
-          </Button>
-        </div>
-      </div>
-
-      <div class="p-6">
-        <DataTable :columns="columns" :data="electronics" />
-      </div>
+  <div class="bg-white rounded-lg shadow-lg">
+    <div class="px-6 py-2 border-b border-gray-200 flex justify-between">
+      <h1 class="text-2xl font-bold text-gray-800">Data Aset Elektronik</h1>
+      <Button @click="refresh()" variant="secondary">Refresh</Button>
+    </div>
+    <div class="px-6 py-2">
+      <DataTable :columns="columns" :data="result?.data || []" :dataStatus="status" />
     </div>
   </div>
 </template>
